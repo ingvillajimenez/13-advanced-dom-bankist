576e3c02be6ec0d00b75e5afe14562ff008c43d3
Juan Villa <ing.villa.jimenez@gmail.com>
2023-12-13 17:43:43 -0600

    Project: 'Bankist' Website

---
+++ script.js
@@ -0,0 +1,34 @@
+"use strict";
+
+///////////////////////////////////////
+// Project: "Bankist" Website
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+
+const openModal = function (e) {
+  e.preventDefault(); // prevent default anchor behavior to jump the page because of hfer="#"
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal)); // Use forEach to attach an event listener to each button
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
